bd689aad1f0a60a4852d448504e3c627c1f55ff3
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-07-09 13:21:29 -0700

    Refactor EntertainmentCenter into class; update docs/data

--- src/entertainment_center.py
+++ src/entertainment_center.py
@@ -6,18 +6,41 @@ from media import Movie
 from fresh_tomatoes import create_movie_tiles_content, open_movies_page
 
 
-movie_data_path = os.path.join(
-    os.path.dirname(__file__),'../data/movies.json')
-with open(movie_data_path, 'r') as f:
-    movie_data = json.load(f)
+class EntertainmentCenter():
+    """
+    Loads movie data and builds a list of Movie objects.
+    """
+    def __init__(self):
+        self.movie_data_path = os.path.join(
+            os.path.dirname(__file__),
+            '../data/movies.json')
 
-movie_list = []
+    def load_data(self, data_path):
+        """
+        Read movie data from file and convert to json.
+        """
+        with open(data_path, 'r') as f:
+            movie_data = json.load(f)
+            return movie_data
 
-for index in range(0, len(movie_data)):
-    new_movie = Movie(**movie_data[index])
-    movie_list.append(new_movie)
+    def create_movie_list(self, movie_data):        
+        """
+        Build a list of movies based on a json-formatted array of movies.
+        """
+        self.movie_list = []
+
+        for index in range(0, len(movie_data)):
+            new_movie = Movie(**movie_data[index])
+            self.movie_list.append(new_movie)
+            
+        return self.movie_list
 
 if __name__ == '__main__':
+    movies = EntertainmentCenter()
+    movie_data = movies.load_data(movies.movie_data_path)
+    movie_list = movies.create_movie_list(movie_data)
+
+    # Build HTML and open page
     open_movies_page(movie_list)
 
 
--- src/media.py
+++ src/media.py
@@ -2,18 +2,29 @@
 
 class Movie(object):
     """
-    Creates a Movie object:
+    Creates a Movie object.
 
-    Valid parameters are:
+    Required parameters:
 
-    title
-    poster_image_url
-    trailer_youtube_url
+    1. id (cannot be None)
+
+    Optional parameters:
+
+    1. title
+    2. poster_image_url
+    3. trailer_youtube_url
+    4. synopsis
+    5. critics_rating
+    6. mpaa_rating
+    7. year
     """
     
     def __init__(self, default=None, **kwargs):
-        
-
+        self.id = kwargs.get('id', default)
         self.title = kwargs.get('title', default)
         self.poster_image_url = kwargs.get('poster_image_url', default)
         self.trailer_youtube_url = kwargs.get('trailer_youtube_url', default)
+        self.synopsis = kwargs.get('synopsis', default)
+        self.critics_rating = kwargs.get('critics_rating', default)
+        self.mpaa_rating = kwargs.get('mpaa_rating', default)
+        self.year = kwargs.get('year', default)
